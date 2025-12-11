5105919ee0a146e3746df54539294c464c0d065a
xenodium <8107219+xenodium@users.noreply.github.com>
2025-12-11 10:01:29 +0000

    Bump shell-maker requirement #142

--- agent-shell.el
+++ agent-shell.el
@@ -5,7 +5,7 @@
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/agent-shell
 ;; Version: 0.24.2
-;; Package-Requires: ((emacs "29.1") (shell-maker "0.84.2") (acp "0.8.1"))
+;; Package-Requires: ((emacs "29.1") (shell-maker "0.84.4") (acp "0.8.1"))
 
 (defconst agent-shell--version "0.24.2")
 
@@ -46,7 +46,7 @@
 (require 'json)
 (require 'map)
 (unless (require 'markdown-overlays nil 'noerror)
-  (error "Please update 'shell-maker' to v0.84.2 or newer"))
+  (error "Please update 'shell-maker' to v0.84.4 or newer"))
 (require 'shell-maker)
 (require 'markdown-overlays)
 (require 'project)
@@ -1471,8 +1471,8 @@ See `agent-shell-make-agent-config' for config format.
 
 Set NO-FOCUS to start in background.
 Set NEW-SESSION to start a separate new session."
-  (unless (version<= "0.84.2" shell-maker-version)
-    (error "Please update shell-maker to version 0.84.2 or newer"))
+  (unless (version<= "0.84.4" shell-maker-version)
+    (error "Please update shell-maker to version 0.84.4 or newer"))
   (unless (version<= "0.8.1" acp-package-version)
     (error "Please update acp.el to version 0.8.1 or newer"))
   (with-temp-buffer ;; client-maker needs a buffer (use a temp one)
